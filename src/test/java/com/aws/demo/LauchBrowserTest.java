98205e3ab7dc0addd25191d37f1ac02bfe511265
Kamran <112368797+KamranRaza12@users.noreply.github.com>
2023-04-26 15:44:38 +0530

    new changes

--- src/test/java/com/aws/demo/LauchBrowserTest.java
+++ src/test/java/com/aws/demo/LauchBrowserTest.java
@@ -4,6 +4,8 @@ package com.aws.demo;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.Test;
 
 public class LauchBrowserTest {
@@ -16,13 +18,19 @@ public class LauchBrowserTest {
     	//System.setProperty("webdriver.chrome.driver", chromeDriverPath);
     	//WebDriver driver = new ChromeDriver();
     	//System.setProperty("webdriver.chrome.driver", ".\\Drivers\\chromedriver.exe");
+    	//System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+    	System.setProperty("webdriver.gecko.driver","/usr/local/bin/");
+    	//ChromeOptions options = new ChromeOptions();
+        //options.addArguments("headless");
+        //options.addArguments("disable-gpu");
+        //driver = new ChromeDriver(options);
     	
-    	System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-    	
-    	ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");
-        options.addArguments("disable-gpu");
-        driver = new ChromeDriver(options);
+    	//Set Firefox Headless mode as TRUE
+        FirefoxOptions options = new FirefoxOptions();
+        options.setHeadless(true);
+        
+        //pass the options parameter in the Firefox driver declaration
+        driver = new FirefoxDriver(options);
         driver.get("https://www.google.com");
         System.out.println("Title of the page is: " + driver.getTitle());
         System.out.println("New Updates to be printed on console");
